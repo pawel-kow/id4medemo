4a9edfb0e7a20dabb3bdd7055c9df66912651790
pawel-kow <pawel-kow@users.noreply.github.com>
2019-05-16 09:00:45 +0200

    NEW VERSION: updated to v3 version from button

--- id4medemo/static/id4medemo/button/javascript.js
+++ id4medemo/static/id4medemo/button/javascript.js
@@ -1,6 +1,11 @@
 
 function setCookie(cname, cvalue) {
-document.cookie = cname + "=" + cvalue + ";path=/";
+	document.cookie = cname + "=" + cvalue + ";path=/";
+}
+
+/* delete cookie needs improvement, does only work on cookies in top home directory where path=/ */
+function delete_cookie( name ) {
+	document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
 }
 
 function getCookie(cname) {
@@ -25,20 +30,35 @@ $(function() {
 		// Cancel the default action, if needed
 		event.preventDefault();
 		// Trigger the button element with a click
-		location.href = $('#id4me-button').attr("data-link") + "&id4me=" + $('#id4me-input').val()  + "&rememberme=true";
+		location.href = $('#id4me-button-anchor').attr("data-link") + "&id4me=" + $('#id4me-input').val()  + "&rememberme=true";
 	  }
+	});
+});
+
+$(function() {
+	$('#id4me-input-signin').click(function(event){
+		console.log("drin");
+		event.preventDefault();
+		// Trigger the button element with a click
+		location.href = $('#id4me-button-anchor').attr("data-link") + "&id4me=" + $('#id4me-input').val()  + "&rememberme=true";
 	});
 });
 
 
 $(document).ready(function(){ 
 	var user = getCookie("id4me.user");
-	/* uncomment next line to test with existing id4me user */
-	//user = "test";
+
 	if (user != "") {      
-	  $('#id4me-button').attr("href", $('#id4me-button').attr("data-link") + "&id4me=" + user)
+	  $('#id4me-button-anchor').attr("href", $('#id4me-button-anchor').attr("data-link") + "&id4me=" + user)
 	  $('#id4me-text').html(" Login as " + user);
 	  $('#id4me-handler').show();
+	  $('#id4me-remove-id').click(function() {
+		  delete_cookie('id4me.user');
+		  $('#id4me-text').html(" Login with ID4me ");
+		  $('#id4me-input').show();
+		  $('#id4me-handler-form').show();
+		  $('#id4me-input').focus();
+	  });
 	} else {
 	  $('#id4me-handler').hide();
 	  $('#id4me-button').click(function() {
